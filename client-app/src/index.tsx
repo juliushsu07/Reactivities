9220bb22da43d93acead1a57c62ddae114224abb
Julius Hsu <Juliushsu07@gmail.com>
2023-03-15 15:43:26 -0400

    Add CRUD functionality in frontend react app.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css'
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
