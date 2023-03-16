8d6062b4c5ef1e23765d51379b5d1de8c482681f
Julius Hsu <Juliushsu07@gmail.com>
2023-03-16 13:05:12 -0400

    Connect frontend CRUD functions with backend through axios and add page loading functionality.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,10 +15,11 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean; 
 }
 
 export default function ActivityDashboard({activities, selectedActivity, deleteActivity,
-  selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
+  selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, submitting}: Props) {
   return (
     <Grid>
         <Grid.Column width= '10'>
@@ -26,6 +27,7 @@ export default function ActivityDashboard({activities, selectedActivity, deleteA
               activities={activities} 
               selectActivity={selectActivity} 
               deleteActivity={deleteActivity}
+              submitting={submitting}
             />
         </Grid.Column>
         <Grid.Column width= '6'>
@@ -36,7 +38,12 @@ export default function ActivityDashboard({activities, selectedActivity, deleteA
               openForm={openForm}
             />}
             {editMode &&
-              <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+              <ActivityForm 
+                closeForm={closeForm} 
+                activity={selectedActivity} 
+                createOrEdit={createOrEdit}
+                submitting={submitting}
+              />}
         </Grid.Column>
     </Grid>
   )
